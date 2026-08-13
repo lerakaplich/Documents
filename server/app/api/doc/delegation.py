efd2791038f8564bf106c3fba44a008dca54d3ef
Alena <alnessher1317@gmail.com>
2026-08-13 14:24:08 +0300

    массовое делегирование + логи комментариев, типов документов, круд доков

--- server/app/api/doc/delegation.py
+++ server/app/api/doc/delegation.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from server.app.deps import get_current_user, get_delegation_service
 from server.app.schemas.doc.document_dto import (
-    RedirectHistoryRead
+    RedirectHistoryRead, BulkDelegateCreate
 )
 from server.app.schemas.user_schemas.employee_dto import CurrentUser
 
@@ -17,25 +17,33 @@ router = APIRouter(prefix="/delegation", tags=["Documents"])
 
 @router.get("/{document_id}/history", response_model=list[RedirectHistoryRead])
 async def get_redirect_history(
-        document_id: int,
-        current_user: CurrentUser = Depends(get_current_user),
-        service: DelegationService = Depends(get_delegation_service)
+    document_id: int,
+    current_user: CurrentUser = Depends(get_current_user),
+    service: DelegationService = Depends(get_delegation_service)
 ):
     """История движений документа"""
     return await service.get_history(document_id)
 
 
 @router.post("/{document_id}", status_code=status.HTTP_201_CREATED)
-async def add_delegate(
-        document_id: int,
-        to_employee_id: int, # В теле запроса (Pydantic модель)
-        message: Optional[str] = None,
-        current_user: CurrentUser = Depends(get_current_user),
-        service: DelegationService = Depends(get_delegation_service)
+async def add_delegates(
+    document_id: int,
+    data: BulkDelegateCreate,
+    current_user: CurrentUser = Depends(get_current_user),
+    service: DelegationService = Depends(get_delegation_service)
 ):
-    """Назначить сотрудника участником/делегатом"""
-    await service.add_delegate(document_id, current_user, to_employee_id, message)
-    return {"status": "success", "message": "Сотрудник добавлен в список участников"}
+    """Назначить одного или нескольких сотрудников делегатами"""
+    added_ids = await service.add_delegates_bulk(
+        doc_id=document_id,
+        actor=current_user,
+        target_ids=data.target_ids,
+        message=data.message
+    )
+    return {
+        "status": "success",
+        "message": f"Успешно добавлено участников: {len(added_ids)}",
+        "added_employee_ids": added_ids
+    }
 
 @router.delete("/{document_id}/{emp_id}")
 async def remove_delegate(
